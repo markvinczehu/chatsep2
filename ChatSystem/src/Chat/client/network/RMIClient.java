ce6b9f3ac56ddeb514c651cff4f3b03f19068a4e
Jimant2 <jimantoniou2@gmail.com>
2020-07-22 21:43:56 +0200

    RMI Client methods

--- ChatSystem/src/Chat/client/network/RMIClient.java
+++ ChatSystem/src/Chat/client/network/RMIClient.java
@@ -2,6 +2,21 @@ package Chat.client.network;
 
 import Chat.shared.networking.ClientCallback;
 
+import java.rmi.RemoteException;
+
 public class RMIClient implements Client, ClientCallback
 {
+  public static void main(String[] args) throws RemoteException
+  {
+
+  }
+
+  public RMIClient (String host)
+  {
+
+  }
+  public void start()
+  {
+
+  }
 }
