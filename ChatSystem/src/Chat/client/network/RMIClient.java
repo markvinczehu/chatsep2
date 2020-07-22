0a5bf341a0bf831cba5d1083d206b4cbf634e72d
Jimant2 <jimantoniou2@gmail.com>
2020-07-22 22:55:46 +0200

    RMI Client methods

--- ChatSystem/src/Chat/client/network/RMIClient.java
+++ ChatSystem/src/Chat/client/network/RMIClient.java
@@ -1,22 +1,38 @@
 package Chat.client.network;
 
 import Chat.shared.networking.ClientCallback;
+import Chat.shared.networking.RMIServer;
 
+import java.rmi.NotBoundException;
+import java.rmi.RMISecurityManager;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 public class RMIClient implements Client, ClientCallback
 {
-  public static void main(String[] args) throws RemoteException
+  public static void main(String[] args)
+      throws RemoteException, NotBoundException
   {
+    System.setProperty("java.security.policy", "security.policy");
+
+    if (System.getSecurityManager() == null)
+    {
+      System.setSecurityManager(new RMISecurityManager());
+    }
+
+    RMIClient client = new RMIClient();
+  }
+
+  private RMIServer server;
+  public RMIClient () {
 
   }
 
-  public RMIClient (String host)
+  public void start() throws RemoteException, NotBoundException
   {
-
-  }
-  public void start()
-  {
+    Registry registry = LocateRegistry.getRegistry("localhost", 1099);
+    server = (RMIServer) registry.lookup("Chat");
 
   }
 }
