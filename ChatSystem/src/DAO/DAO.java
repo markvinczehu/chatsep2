65fbee91bfe67193922fbc32f814e6cccab97135
ajurj <ajurj12@yahoo.com>
2020-07-23 18:53:52 +0200

    made some tests to check the database connection - Alex

--- ChatSystem/src/DAO/DAO.java
+++ ChatSystem/src/DAO/DAO.java
@@ -1,5 +1,11 @@
 package DAO;
 
+import java.sql.SQLException;
+
 public interface DAO
 {
+  void create(String name, int age) throws SQLException;
+  void updateName(String name) throws SQLException;
+  void updateAge(int age) throws SQLException;
+  void delete(String name) throws SQLException;
 }
--- ChatSystem/src/DAO/DAOImpl.java
+++ ChatSystem/src/DAO/DAOImpl.java
@@ -1,5 +1,56 @@
 package DAO;
 
-public class DAOImpl
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DAOImpl implements DAO
 {
+  private static DAOImpl instance;
+
+  private DAOImpl() throws SQLException
+  {
+    DriverManager.registerDriver(new org.postgresql.Driver());
+  }
+
+  public static synchronized DAOImpl getInstance() throws SQLException
+  {
+    if(instance == null)
+    {
+      instance = new DAOImpl();
+    }
+    return instance;
+  }
+
+  private Connection getConnection() throws SQLException
+  {
+    return DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres?currentSchema=test", "postgres", "293150");
+  }
+
+  @Override public void create(String name, int age) throws SQLException
+  {
+    try(Connection connection = getConnection()){
+      PreparedStatement statement = connection.prepareStatement(
+          "insert into persontest(name, age) values (?, ?);");
+      statement.setString(1, name);
+      statement.setInt(2, age);
+      statement.executeUpdate();
+    }
+  }
+
+  @Override public void updateName(String name) throws SQLException
+  {
+
+  }
+
+  @Override public void updateAge(int age) throws SQLException
+  {
+
+  }
+
+  @Override public void delete(String name) throws SQLException
+  {
+
+  }
 }
