e090904ea6c51567711f661a516625b7f3282b84
markvincze <mark.vincze.hu@gmail.com>
2020-07-23 10:42:38 +0200

    RegistrationController skeleton ---- Mark

---
+++ ChatSystem/src/Chat/client/view/registration/RegistrationController.java
@@ -0,0 +1,40 @@
+package Chat.client.view.registration;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class RegistrationController {
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private TextField userName;
+
+    @FXML
+    private Button resetButton;
+
+    @FXML
+    private Button registerButton;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    void backToLogin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void createAccount(ActionEvent event) {
+
+    }
+
+    @FXML
+    void resetData(ActionEvent event) {
+
+    }
+
+}
+
